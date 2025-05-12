1bb3f89830f3bb67ec4a7476cf349c14a3668823
romakot321 <play62342@gmail.com>
2025-05-12 20:47:45 +0300

    Try to fix prompt edit in admin

--- src/app/db/admin/views.py
+++ src/app/db/admin/views.py
@@ -1,4 +1,6 @@
-from fastapi import UploadFile
+import io
+from fastapi import Request
+from starlette.datastructures import FormData, UploadFile
 from app.db.tables import Task, Prompt, TaskItem, TaskImage, PromptUserInput
 from sqladmin import ModelView
 from sqladmin.formatters import Markup
@@ -34,6 +36,32 @@ class PromptView(ModelView, model=Prompt):
     column_formatters = {"image": format_image_url}
     column_formatters_detail = {"image": format_image_url}
     form_overrides = dict(image=FileField)
+
+    async def _handle_form_data(self, request: Request, obj = None) -> FormData:
+        """
+        Handle form data and modify in case of UploadFile.
+        This is needed since in edit page
+        there's no way to show current file of object.
+        """
+
+        form = await request.form()
+        form_data: list[tuple[str, str | UploadFile]] = []
+        for key, value in form.multi_items():
+            if not isinstance(value, UploadFile):
+                form_data.append((key, value))
+                continue
+
+            should_clear = form.get(key + "_checkbox")
+            empty_upload = len(await value.read(1)) != 1
+            await value.seek(0)
+            if should_clear:
+                form_data.append((key, UploadFile(io.BytesIO(b""))))
+            elif empty_upload and obj and getattr(obj, key):
+                f = getattr(obj, key)  # In case of update, imitate UploadFile
+                form_data.append((key, UploadFile(filename="tmp.png", file=f.open())))
+            else:
+                form_data.append((key, value))
+        return FormData(form_data)
 
     async def on_model_change(self, data: dict, model, is_created, request):
         if is_created:
